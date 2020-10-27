fc2811f5801471a91973c4dd62932c48c19a557e
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2020-10-26 23:46:41 -0500

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,6 +1,7 @@
 node default {
-  file {'root/README':
+  file { '/root/README':
     ensure => file,
-     content =>'this is a readme',
-}
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
