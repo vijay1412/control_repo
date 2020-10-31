27f5885cda0e69513a2abe3b1d859162ee365f43
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2020-10-30 23:18:57 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,3 +5,13 @@ node default {
     owner   => 'root',
   }
 }
+node 'master.puppet.vm'
+  include role::master_server
+}
+
+node /*web/ {
+include role::app_server
+}
+node /*db/ {
+include role::db_server
+}
