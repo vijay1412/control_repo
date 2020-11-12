23594169d966130fec5735ffca423673c0921dc4
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2020-11-12 14:48:40 -0600

    Create init.pp

---
+++ site/Wildfly/manifests/init.pp
@@ -0,0 +1,10 @@
+class Wildfly {
+file {'/var/opt/wildfly' :
+  ensure => directory,
+  }
+
+ file {'/var/opt/wildfly/wildfly-14.0.1.Final.tar.gz':
+ ensure =>file
+ source => https://download.jboss.org/wildfly/14.0.1.Final/wildfly-14.0.1.Final.tar.gz
+ }
+ }
