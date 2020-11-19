ff4535cc4d886cd9dda4063ea38952a154307039
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2020-11-18 22:20:17 -0600

    Update init.pp

--- site/wildfly/manifests/init.pp
+++ site/wildfly/manifests/init.pp
@@ -5,7 +5,7 @@ file {'/var/opt/wildfly' :
 
  file {'/var/opt/wildfly/wildfly-14.0.1.Final.tar.gz':
  ensure =>file
- source => https://download.jboss.org/wildfly/14.0.1.Final/wildfly-14.0.1.Final.tar.gz
+ source => 'https://download.jboss.org/wildfly/14.0.1.Final/wildfly-14.0.1.Final.tar.gz',
  }
  package {'java':
  ensure => present,
