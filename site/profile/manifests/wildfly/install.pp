9e0a727c48c1f1d361d8cb27fb223a5b9f34f74f
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2020-11-20 16:52:48 -0600

    Update install.pp

--- site/profile/manifests/wildfly/install.pp
+++ site/profile/manifests/wildfly/install.pp
@@ -1,11 +1,11 @@
 class profile::wildfly::install  {
- } {
+ }
     $install_source = $profile::wildfly::install_source
     $install_file = basename($install_source)
 
     file { "${wildfly::install_cache_dir}/${install_file}":
       source => $install_source,
-}
+
     # Gunzip+Untar wildfly.tar.gz if download was successful.
      #exec { "untar ${install_file}":
       #command  => "tar --no-same-owner --no-same-permissions --strip-components=1 -C ${wildfly::dirname} -zxvf ${wildfly::install_cache_dir}/${install_file}",
