71d5b3ff8f49984c3b8a0cfd4594dcc279001bed
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2020-11-20 16:25:06 -0600

    Update install.pp

--- site/profile/manifests/wildfly/install.pp
+++ site/profile/manifests/wildfly/install.pp
@@ -1,11 +1,20 @@
-# Downloads and installs Wildfly from a remote source or a system package.
-class profile::wildfly::install {
-file {'/var/opt/wildfly' :
-  ensure => directory,
+ Downloads and installs Wildfly from a remote source or a system package.
+class wildfly::install  {
+  } {
+    $install_source = $wildfly::install_source
+    $install_file = basename($install_source)
+
+    file { "${wildfly::install_cache_dir}/${install_file}":
+      source => $install_source,
+    }
+
+    # Gunzip+Untar wildfly.tar.gz if download was successful.
+    ~> exec { "untar ${install_file}":
+      command  => "tar --no-same-owner --no-same-permissions --strip-components=1 -C ${wildfly::dirname} -zxvf ${wildfly::install_cache_dir}/${install_file}",
+      path     => ['/bin', '/usr/bin', '/sbin'],
+    }
   }
-wildfly_download {
- source_url  => 'https://download.jboss.org/wildfly/14.0.1.Final/wildfly-14.0.1.Final.tar.gz',
-destination_directory => /var/opt/wildfly
- }
+}
+
  
 
