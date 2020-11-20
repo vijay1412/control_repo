b0e50fc6e27a8eac3d506420ede485a054e2b8ae
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2020-11-19 23:40:32 -0600

    Update install.pp

--- site/profile/manifests/wildfly/install.pp
+++ site/profile/manifests/wildfly/install.pp
@@ -6,13 +6,11 @@ class profile::wildfly::install(
 
     file { "${wildfly::install_cache_dir}/${install_file}":
       source => $install_source,
-    }
 
     # Gunzip+Untar wildfly.tar.gz if download was successful.
     ~> exec { "untar ${install_file}":
       command  => "tar --no-same-owner --no-same-permissions --strip-components=1 -C ${wildfly::dirname} -zxvf ${wildfly::install_cache_dir}/${install_file}",
-      path     => ['/bin', '/usr/bin', '/sbin'],
-      loglevel => 'notice',
-      creates  => "${wildfly::dirname}/jboss-modules.jar",
+      path     => ['/bin', '/usr/bin', '/sbin']
+      }
     
 
