83d10b7e78ae3ff9a14243b20989e5b6668b32ef
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2020-11-20 18:59:55 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -1,7 +1,7 @@
 class profile::wildfly::server::install (
 ){
 #Download 
-archive { "wildfly-14.0.1.Final.tar.gz":
+archive { "${profile::wildfly::server::wildfly_home}\\${profile::wildfly::server::wildfly-14.0.1.Final.tar.gz}":
 ensure => present ,
 source => "https://download.jboss.org/wildfly/14.0.1.Final/wildfly-14.0.1.Final.tar.gz",
 target => $profile::wildfly::server::wildfly_home,
