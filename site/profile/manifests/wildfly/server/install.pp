c091c0b38073a2102a16ea612a7677e1d9fc000c
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-01 22:29:02 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -3,5 +3,12 @@ class { 'wildfly':
   version        => '14.0.0',
   install_source => 'http://download.jboss.org/wildfly/14.0.0.Final/wildfly-14.0.0.Final.tar.gz',
 }
+wildfly::undertow::https { 'https':
+  socket_binding    => 'https',
+  keystore_path     => '/vagrant/identitystore.jks',
+  keystore_password => 'changeit',
+  key_alias         => 'demo',
+  key_password      => 'changeit'
+}
 
 }
