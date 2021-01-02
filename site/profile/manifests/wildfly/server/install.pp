cf4148ae6e1b637040e5786167e73fe95c019477
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-01 22:14:20 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -1,18 +1,8 @@
 class profile::wildfly::server::install {
-wildfly::undertow::https { 'https':
-  socket_binding    => 'https',
-  keystore_path     => '/vagrant/identitystore.jks',
-  keystore_password => 'changeit',
-  key_alias         => 'demo',
-  key_password      => 'changeit'
-}
-wildfly::web::ssl { 'ssl':
-  connector            => 'https',
-  protocol             => 'TLSv1,TLSv1.1,TLSv1.2',
-  password             => 'changeit',
-  key_alias            => 'demo',
-  certificate_key_file => '/opt/identitykeystore.jks',
-  cipher_suite         => 'TLS_RSA_WITH_AES_128_CBC_SHA,SSL_RSA_WITH_3DES_EDE_CBC_SHA',
+wildfly::resource { "/system-property=myproperty":
+  content => {
+    'value' => '1234'
+  },
 }
 
 }
