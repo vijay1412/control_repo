38e549a85a84c4fcf572f19ab89d61e8a8a5bc33
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-01 22:20:01 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -1,6 +1,6 @@
 class profile::wildfly::server::install {
 class { 'wildfly':
-  version        => '10.1.0',
+  version        => '14.0.0',
   install_source => 'http://download.jboss.org/wildfly/14.0.0.Final/wildfly-14.0.0.Final.tar.gz',
 }
 
