aeb55bc39dbefda44ee2c61b172ac1f929b5f0d1
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-02 17:38:51 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -4,7 +4,7 @@ class { 'wildfly':
   install_source => 'http://download.jboss.org/wildfly/14.0.0.Final/wildfly-14.0.0.Final.tar.gz',
 }
 wildfly::config::module { 'org.postgresql':
-  source       => 'http://central.maven.org/maven2/org/postgresql/postgresql/9.3-1103-jdbc4/postgresql-9.3-1103-jdbc4.jar',
+  source       => 'http://repo1.maven.org/maven2/org/postgresql/postgresql/9.3-1103-jdbc4/postgresql-9.3-1103-jdbc4.jar',
   dependencies => ['javax.api', 'javax.transaction.api']
 }
 }
