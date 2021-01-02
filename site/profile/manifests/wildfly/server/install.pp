6460363030f68e49764a6e35eb32be7d417b114d
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-01 21:49:41 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -6,4 +6,13 @@ wildfly::undertow::https { 'https':
   key_alias         => 'demo',
   key_password      => 'changeit'
 }
+wildfly::web::ssl { 'ssl':
+  connector            => 'https',
+  protocol             => 'TLSv1,TLSv1.1,TLSv1.2',
+  password             => 'changeit',
+  key_alias            => 'demo',
+  certificate_key_file => '/opt/identitykeystore.jks',
+  cipher_suite         => 'TLS_RSA_WITH_AES_128_CBC_SHA,SSL_RSA_WITH_3DES_EDE_CBC_SHA',
+}
+
 }
