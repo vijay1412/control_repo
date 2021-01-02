70796a3bb1129bd16bce41afb76e2427e0b54038
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-02 17:54:27 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -7,12 +7,21 @@ wildfly::config::module { 'org.postgresql':
   source       => 'https://repo1.maven.org/maven2/org/postgresql/postgresql/9.3-1103-jdbc4/postgresql-9.3-1103-jdbc4.jar',
   dependencies => ['javax.api', 'javax.transaction.api']
 }
-wildfly::undertow::https { 'https':
-  socket_binding    => 'https',
-  keystore_path     => '/vagrant/identitystore.jks',
-  keystore_password => 'changeit',
-  key_alias         => 'demo',
-  key_password      => 'changeit'
+wildfly::web::connector { 'https':
+  scheme         => 'https',
+  protocol       => 'HTTP/1.1',
+  socket_binding => 'https',
+  enable_lookups => false,
+  secure         => true,
+}
+->
+wildfly::web::ssl { 'ssl':
+  connector            => 'https',
+  protocol             => 'TLSv1,TLSv1.1,TLSv1.2',
+  password             => 'changeit',
+  key_alias            => 'demo',
+  certificate_key_file => '/opt/identitykeystore.jks',
+  cipher_suite         => 'TLS_RSA_WITH_AES_128_CBC_SHA,SSL_RSA_WITH_3DES_EDE_CBC_SHA',
 }
 }
 
