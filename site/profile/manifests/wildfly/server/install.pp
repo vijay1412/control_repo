50394f6024e8c992813d5b08c8f0d9fee9abd920
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-04 15:29:11 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -4,12 +4,20 @@ class { 'wildfly':
   install_source => 'http://download.jboss.org/wildfly/14.0.0.Final/wildfly-14.0.0.Final.tar.gz',
   java_opts      => '-Djava.net.preferIPv4Stack=true'
 }
-wildfly::undertow::https { 'https':
-  socket_binding    => 'https',
-  keystore_path     => '/vagrant/identitystore.jks',
-  keystore_password => 'changeit',
-  key_alias         => 'demo',
-  key_password      => 'changeit'
+wildfly::web::connector { 'https':
+  scheme         => 'https',
+  protocol       => 'HTTP/1.1',
+  socket_binding => 'https',
+  enable_lookups => false,
+  secure         => true,
 }
+->
+wildfly::web::ssl { 'ssl':
+  connector            => 'https',
+  protocol             => 'TLSv1,TLSv1.1,TLSv1.2',
+  password             => 'changeit',
+  key_alias            => 'demo',
+  certificate_key_file => '/opt/identitykeystore.jks',
+  cipher_suite         => 'TLS_RSA_WITH_AES_128_CBC_SHA,SSL_RSA_WITH_3DES_EDE_CBC_SHA',
 }
 
