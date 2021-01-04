deef759e473ec8d6ef342e8044fa0eaba9e55b16
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-04 14:41:23 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -2,6 +2,7 @@ class profile::wildfly::server::install {
 class { 'wildfly':
   version        => '14.0.0',
   install_source => 'http://download.jboss.org/wildfly/14.0.0.Final/wildfly-14.0.0.Final.tar.gz',
+  java_opts      => 'java.net.preferIPv4Stack=true'
 }
 wildfly::undertow::https { 'https':
   socket_binding    => 'https',
