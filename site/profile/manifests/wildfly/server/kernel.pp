2b61fa33b7b4697702468d2d716dbe447519e1a6
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-06 19:30:21 -0600

    Create kernel.pp

---
+++ site/profile/manifests/wildfly/server/kernel.pp
@@ -0,0 +1,27 @@
+############################################################################################################################################
+# PROFILE TO MANAGE Tomcat Apache Kernel Settings
+############################################################################################################################################
+class profile::apache::tomcat::kernel(
+  $vm_swappiness = '10',
+  $vm_min_free_kbytes = '67584',
+) {
+
+  exec { 'tomcat-sysctl-refresh':
+    command     => '/sbin/sysctl --system',
+    subscribe   => [
+      File['/etc/sysctl.d/90-tomcat.conf'],
+    ],
+    refreshonly => true,
+  }
+
+  # Set kernel settings for app
+  file { '/etc/sysctl.d/90-tomcat.conf':
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => epp('profile/apache/tomcat/sysctl/90-tomcat.conf.epp'),
+  }
+
+}
+
