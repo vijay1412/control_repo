9be6b5fe6735fff035eece54d4e5ee1e3ba63d78
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-06 19:49:56 -0600

    Update server.pp

--- site/profile/manifests/wildfly/server.pp
+++ site/profile/manifests/wildfly/server.pp
@@ -21,8 +21,6 @@ class profile::wildfly::server (
   Integer $shutdown_wait                                      = 30,
   Integer $install_download_timeout                           = 500,
   ) {
-  contain ::profile::wildfly::server::kernel
   contain ::profile::wildfly::server::install
-     Class['profile::wildfly::server::kernel']
-  -> Class['profile::wildfly::server::install']
+     Class['profile::wildfly::server::install']
   }
