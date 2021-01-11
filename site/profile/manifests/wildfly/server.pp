8f09174085121c448cd8b08130a85f0a1ed0a4a7
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-10 23:37:01 -0600

    Update server.pp

--- site/profile/manifests/wildfly/server.pp
+++ site/profile/manifests/wildfly/server.pp
@@ -23,7 +23,8 @@ class profile::wildfly::server (
   ) {
    contain ::profile::wildfly::server::filesystems
    contain ::profile::wildfly::server::install
+   contain ::profile::wildfly::server::services
         Class['profile::wildfly::server::filesystems']
      -> Class['profile::wildfly::server::install']
-     -> Class['profile::wildfly::server::services']
+     -> Class['profile::wildfly::server::services']          
   }
