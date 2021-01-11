68cecc23dcc6b224ff9c973e7dacad48ec104a6a
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-11 00:07:38 -0600

    Update services.pp

--- site/profile/manifests/wildfly/server/services.pp
+++ site/profile/manifests/wildfly/server/services.pp
@@ -1,7 +1,7 @@
 class profile::wildfly::server::services(
 ){
 class {'wildfly':
-conf_file      => '/etc/default/wildfly.conf'
+conf_file      => '/etc/default/wildfly.conf',
 conf_template  => 'wildfly/wildfly.sysvinit.conf.epp',
     }
     }
