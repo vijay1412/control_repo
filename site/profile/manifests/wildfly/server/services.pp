9bc3e93e1764ceca16b51e1ee8383b9ce68cd4a6
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-10 23:32:01 -0600

    Update services.pp

--- site/profile/manifests/wildfly/server/services.pp
+++ site/profile/manifests/wildfly/server/services.pp
@@ -1,7 +1,10 @@
- file { '/etc/default/wildfly.conf':
+class profile::wildfly::server::services(
+){
+file { '/etc/default/wildfly.conf':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
     content => epp('profile/wildfly/server/wildfly.sysvinit.conf.epp'),
-  }
+    }
+    }
