0efa7b9aab56a6b8fd2363c318d755d141167a27
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-11 17:24:24 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -3,12 +3,11 @@ class { 'wildfly':
   version        => '14.0.0',
   install_source => 'http://download.jboss.org/wildfly/14.0.0.Final/wildfly-14.0.0.Final.tar.gz',
   properties       => {
-    'jboss.bind.address'            => $profile::wildfly::server::$public_bind,
-    'jboss.bind.address.management' => $profile::wildfly::server::$mgmt_bind,
+    'jboss.bind.address'            => 0.0.0.0
+    'jboss.bind.address.management' => 0.0.0.0
   },
   conf_file      => '/etc/default/wildfly.conf',
   java_opts      => '-Djava.net.preferIPv4Stack=true'
-  
 }
 ~>
 wildfly::restart { 'Restart required':
