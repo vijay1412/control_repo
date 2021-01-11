96452b264a30b1984e069589478d16ccf74f20ab
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-11 00:09:05 -0600

    Delete services.pp

--- site/profile/manifests/wildfly/server/services.pp
+++
@@ -1,7 +0,0 @@
-class profile::wildfly::server::services(
-){
-class {'wildfly':
-conf_file      => '/etc/default/wildfly.conf',
-conf_template  => 'wildfly/wildfly.sysvinit.conf.epp',
-    }
-    }
