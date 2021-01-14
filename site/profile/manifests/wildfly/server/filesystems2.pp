14c7ad309589cb021f4d10457cfe28c97542cd61
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-14 09:52:02 -0600

    Update filesystems2.pp

--- site/profile/manifests/wildfly/server/filesystems2.pp
+++ site/profile/manifests/wildfly/server/filesystems2.pp
@@ -1,10 +1,100 @@
-          owner   => $profile::users::jbo101::user,
+      
+      
+      #
+
+class profile::wildfly::server::filesystems(
+){
+
+  # These need to be created before the mount points
+  file { [
+      $profile::wildfly::server::wildfly_log_base,
+      $profile::wildfly::server::install_cache_dir,
+      ]:
+      ensure => directory,
+      owner  => $profile::users::jbo101::user,
+      group  => $profile::groups::jas::group,
+      mode   => '0755',
+      backup => false,
+  } 
+
+ # These need to be created before the mount points
+  file { [
+      $profile::wildfly::server::wildfly_log_base,
+      $profile::wildfly::server::install_cache_dir,
+      ]:
+      ensure => directory,
+      owner  => $profile::users::jbo101::user,
+      group  => $profile::groups::jas::group,
+      mode   => '0755',
+      backup => false,
+  }
+ # Container, don't create LVMs
+  if $facts['virtual'] == 'docker' {
+    file { [
+        $profile::wildfly::server::wildfly_home_dir,
+        $profile::wildfly::server::server_logs_dir,
+      ]:
+        ensure  => directory,
+        owner   => $profile::users::jbo101::user,
+        group   => $profile::groups::jas::group,
+        mode    => '0755',
+        backup  => false,
+        require => [
+          File[
+            $profile::wildfly::server:: $wildfly_logs,
+          ],
+        ],
+    }
+  }
+
+ # Virtual Machine, Create LVMs, Filesystems and Mounts
+  else {
+    $fs_type = $facts['mountpoints']['/']['filesystem']
+
+    logical_volume { $profile::wildfly::server::wildfly_home_volume_name:
+      ensure       => present,
+      volume_group => $profile::wildfly::server::volume_group,
+      size         => $profile::wildfly::server::wildfly_home_size,
+    }
+
+    exec { "mkdir_${profile::wildfly::server::wildfly_home_dir}":
+      command => "mkdir ${$profile::wildfly::server::wildfly_home_dir}",
+      unless  => "test -d ${$profile::wildfly::server::wildfly_home_dir}",
+      path    => '/usr/bin:/usr/sbin:/bin',
+      before  => Mount[$profile::wildfly::server::wildfly_home_dir],
+      require => [
+        File[
+          $profile::wildfly::server::wildfly_log_base,
+        ],
+      ],
+    }
+  
+   filesystem { $profile::wildfly::server::wildfly_home_filesystem:
+      ensure  => present,
+      fs_type => $fs_type,
+      require => Logical_volume[$profile::wildfly::server::wildfly_home_volume_name],
+    }
+
+    mount { $profile::wildfly::server::wildfly_home_dir:
+      ensure   => mounted,
+      atboot   => true,
+      device   => $profile::wildfly::server::wildfly_home_filesystem,
+      fstype   => $fs_type,
+      options  => 'defaults,noatime',
+      pass     => 2,
+      remounts => true,
+      require  => Filesystem[$profile::wildfly::server::wildfly_home_filesystem],
+    }
+
+    file { $profile::wildfly::server::wildfly_home_dir:
+      ensure  => directory,
+      owner   => $profile::users::jbo101::user,
       group   => $profile::groups::jas::group,
       mode    => '0755',
       backup  => false,
       require => Mount[$profile::wildfly::server::wildfly_home_dir],
     }
-
+      
 logical_volume { $profile::wildfly::server::server_logs_volume_name:
       ensure       => present,
       volume_group => $profile::wildfly::server::volume_group,
