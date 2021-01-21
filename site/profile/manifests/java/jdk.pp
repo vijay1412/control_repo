dbea9b93b89bef7ffe4354b55a16637d01d8dfa7
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-21 16:02:36 -0600

    Update jdk.pp

--- site/profile/manifests/java/jdk.pp
+++ site/profile/manifests/java/jdk.pp
@@ -1,3 +1,23 @@
 ######################
 #jdk
 ########################
+class profile::java::jdk(
+  $version  = '1.8.0',
+  $type     = 'openjdk',
+) {
+  case $type {
+    'openjdk': {
+      case $version {
+        '1.6.0', '1.7.0', '1.8.0', '11': {
+          $java_home = '/usr/lib/jvm/java'
+
+          class { '::profile::java::jdk::openjdk':
+            version   => $version,
+            java_home => $java_home,
+          }
+        }
+        default: {
+          fail("ERROR: Unsupported openjdk version ${version}")
+        }
+      }
+    }
