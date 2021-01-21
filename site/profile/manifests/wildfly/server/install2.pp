cb4259aee9e5da17c55c9bb8de40ba4dae28d499
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-21 17:52:15 -0600

    Create install2.pp

---
+++ site/profile/manifests/wildfly/server/install2.pp
@@ -0,0 +1,39 @@
+############################################
+#WILD FLY ADDING JAVA PROFLE AND INSTALL
+#############################################
+class profile::wildfly::server::install {
+class { 'wildfly':
+  version        => '14.0.0',
+  install_source => 'http://download.jboss.org/wildfly/14.0.0.Final/wildfly-14.0.0.Final.tar.gz',
+  properties       => {
+    'jboss.bind.address'            => '0.0.0.0',
+    'jboss.bind.address.management' => '0.0.0.0',
+    'jboss.management.http.port' => '9990',
+    'jboss.management.https.port' => '9993',
+    'jboss.http.port' => '8080',
+    'jboss.https.port' => '8443',
+    'jboss.ajp.port' => '8009',
+  },
+  java_home      => 'profile::java::jdk::java_home}',
+  conf_file      => '/etc/default/wildfly.conf',
+  java_opts      => '-Djava.net.preferIPv4Stack=true'
+}
+~>
+wildfly::restart { 'Restart required':
+  retries => 2,
+  wait    => 30,
+}
+wildfly::undertow::https { 'https':
+  socket_binding    => 'https',
+  keystore_path     => '/vagrant/identitystore.jks',
+  keystore_password => 'changeit',
+  key_alias         => 'demo',
+  key_password      => 'changeit'
+}
+wildfly::logging::category { 'org.jboss.deployment':
+  level               => 'DEBUG',
+  use_parent_handlers =>  true,
+}
+
+}
+
