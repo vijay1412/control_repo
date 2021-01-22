01f9ba621eba273b7484f340c176204fa3ac91ff
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-21 20:17:38 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -22,6 +22,7 @@ class { 'wildfly':
     'jboss.https.port' => '8443',
     'jboss.ajp.port' => '8009',
   },
+  java_home => '$profile::java::jdk::java_home',
   require => Class['::profile::java::jdk',],
   conf_file      => '/etc/default/wildfly.conf',
   java_opts      => '-Djava.net.preferIPv4Stack=true'
