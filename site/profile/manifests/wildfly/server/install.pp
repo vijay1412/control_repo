b61ff06ce9c5e2e1cb0c8b7b01d039318325ba3d
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-24 23:56:51 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -26,7 +26,7 @@ class { 'wildfly':
   #java_home => '/usr/lib/jvm/java',
   #require => Class['::profile::java::jdk',],
   #conf_file      => '/etc/wildfly/wildfly.conf',
-  conf_template  => '/profile/wildfly/wildfly.systemd.conf.epp',
+  conf_template  => 'profile/wildfly/wildfly.systemd.conf.epp',
   java_opts      => '-Djava.net.preferIPv4Stack=true'
 }
 ~>
