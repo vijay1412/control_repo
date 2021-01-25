dd100a47c486b74126349261f62f818f631520cc
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-24 23:06:36 -0600

    Update server.pp

--- site/profile/manifests/wildfly/server.pp
+++ site/profile/manifests/wildfly/server.pp
@@ -1,6 +1,6 @@
 class profile::wildfly::server (
   Stdlib::Unixpath $wildfly_home                              = '/var/opt/wildfly',
-  jdk_type = 'openjdk',
+  $jdk_type = 'openjdk',
   $jdk_version = '1.8.0',
   Stdlib::Unixpath $console_log                               = '/var/log/wildfly/console.log',
   Stdlib::Unixpath $install_cache_dir                         = '/var/cache/wget',
