8da424b3c94b48a820ff650466f1105935df8b30
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-26 16:42:20 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -47,12 +47,8 @@ wildfly::logging::category { 'org.jboss.deployment':
   use_parent_handlers =>  true,
 }
 wildfly::cli { "max-post-size value":
-   command => "/subsystem=undertow/server=default-server/ajp-listener=ajp:write-attribute(name=max-post-size,value=20000000)",
- # command => "/subsystem=undertow/server=default-server/ajp-listener=ajp:write-attribute(name=max-post-size,value=20000000)",
+     command => "/subsystem=undertow/server=default-server/ajp-listener=ajp:write-attribute
+    # command => "/subsystem=undertow/server=default-server/ajp-listener=ajp:write-attribute(name=max-post-size,value=20000000)",
   }
-  wildfly::cli { "Enable ADatasource":
-  command => "/subsystem=datasources/data-source=ADatasource:enable",
-  unless  => "(result == true) of /subsystem=datasources/data-source=ADatasource:read-attribute(name=enabled)",
-  
-}
+
 }
