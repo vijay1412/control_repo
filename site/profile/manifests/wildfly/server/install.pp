a1a017c5a785c6c91410b7cfa77c3a3c8f6e0555
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-26 11:47:56 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -36,17 +36,17 @@ class { 'wildfly':
  # retries => 2,
   #wait    => 30,
 #}
-#wildfly::undertow::https { 'https':
- # socket_binding    => 'https',
-  #keystore_path     => '/vagrant/identitystore.jks',
-  #keystore_password => 'changeit',
-  #key_alias         => 'demo',
-  #key_password      => 'changeit'
-#}
-#wildfly::logging::category { 'org.jboss.deployment':
- # level               => 'DEBUG',
-  #use_parent_handlers =>  true,
-#}
+wildfly::undertow::https { 'https':
+  socket_binding    => 'https',
+  keystore_path     => '/vagrant/identitystore.jks',
+  keystore_password => 'changeit',
+  key_alias         => 'demo',
+  key_password      => 'changeit'
+}
+wildfly::logging::category { 'org.jboss.deployment':
+  level               => 'DEBUG',
+  use_parent_handlers =>  true,
+}
 
 }
 
