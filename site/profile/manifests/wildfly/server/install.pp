cbdf81a76eb965c61579cf7e85f852dd64cefff4
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-26 19:11:51 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -56,7 +56,7 @@ wildfly::logging::category { 'org.jboss.deployment':
 -> wildfly::resource { '/subsystem=undertow/server=default-server/ajp-listener=ajp':
      #path  => "/subsystem=undertow/server=default-server/ajp-listener=ajp",
   #   operat => {
-        operation-headers  => {
+        peration-headers  => {
           'max-post-size' => '2000',
     },
    }
