cead624fd5fabf393c59b8bdb755b11d442f2274
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-27 10:34:54 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -31,10 +31,7 @@ class { 'wildfly':
   dirname => Stdlib::Unixpath($profile::wildfly::server::wildfly_home),
   #conf_template  => 'profile/wildfly/wildfly.systemd.conf.epp',
   java_opts      => {'-Djava.net.preferIPv4Stack=true',
-                     '-Djava.net.preferIPv4Addresses=true',
-                     "-Djavax.net.ssl.keyStore=${profile::wildfly::server::java_keystore_file}",
-                     "-Djavax.net.ssl.keyStorePassword =${jks_password}",
-                     },
+                     }
                   
 }
 #~>
