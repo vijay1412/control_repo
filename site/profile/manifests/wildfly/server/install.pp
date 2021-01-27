cd36b3f51763307cad4b0899a2cf6719005e22b6
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-27 12:56:41 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -33,7 +33,7 @@ class { 'wildfly':
   #conf_template  => 'profile/wildfly/wildfly.systemd.conf.epp',
    java_opts      =>   "'-Djava.net.preferIPv4Stack=true'
                        '-Djava.net.preferIPv4Addresses=true'
-                       '-Djavax.net.ssl.keyStore=$profile::wildfly::server::java_keystore_file'
+                       '-Djavax.net.ssl.tomcatkeystorefile=$profile::wildfly::server::java_keystore_file'
                         '-Djavax.net.ssl.keyStorePassword =changeit'",
   }
 #~>
@@ -43,7 +43,8 @@ class { 'wildfly':
 #}
 wildfly::undertow::https { 'https':
   socket_binding    => 'https',
-  keystore_path     => ($profile::wildfly::server::java_keystore_file),
+  #keystore_path     => ($profile::wildfly::server::java_keystore_file),
+  keystore_path => $tomcatkeystorefile,
   keystore_password => 'changeit'
 }
 wildfly::logging::category { 'org.jboss.deployment':
