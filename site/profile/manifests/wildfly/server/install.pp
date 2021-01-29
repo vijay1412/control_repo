a32843c5fbbf2f6eb6d11d212b372484cd1c9bd5
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-01-29 14:16:17 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -84,10 +84,5 @@ wildfly::logging::category { 'org.jgroups':
       'generate-strings-as-char-arrays' => true,
         }
        }
-       
-     wildfly::jgroups::stack::tcpgossip { 'TCPGOSSIP':
-     initial_hosts       => '${amfam.nhq.gossip.router}[7777] ,${amfam.grl.gossip.router}[7777]',
-     num_initial_members => 2
-  }
-
+      
    }
