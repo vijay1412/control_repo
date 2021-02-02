d67e147cfb41ae4980ab30d117bed9d647001dfb
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-02-02 10:08:31 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -118,7 +118,7 @@ wildfly::logging::category { 'org.jgroups':
     }
   }
 
-wildfly::resource { "/subsystem=jgroups/stack=tcpgossip/protocol=TCPGOSSIP":
+wildfly::resource { "/subsystem=jgroups/stack=tcpgossip/protocol=org.jgroups.protocols.TCPGOSSIP":
     ensure => present,
     content => {
       'socket-binding' => "jgroups-host-a, jgroups-host-b",
