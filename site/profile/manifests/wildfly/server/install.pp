f2c0b9c715438f597f62174dd38a5af4700e8980
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-02-08 14:05:49 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -179,7 +179,6 @@ wildfly::resource { "/subsystem=jgroups/stack=tcpgossip/protocol=org.jgroups.pro
        }
         
      ->   wildfly::resource {'/subsystem=messaging-activemq/server=default/broadcast-group=bg-group1':
-        ensure => present,
         content => {
        'jgroups-channel'=> 'activemq-cluster-${jboss.partition.name:DefaultPartition}',
        }
