00ac3af52b607e31f4b4866568492ad650956e78
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-02-08 12:51:58 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -166,15 +166,15 @@ wildfly::resource { "/subsystem=jgroups/stack=tcpgossip/protocol=org.jgroups.pro
       'port' => 8888,
         }
        }
-        wildfly::resource {'/subsystem=messaging-activemq/server=default':
-        ensure => present,
+        wildfly::resource {'/subsystem=messaging-activemq/server=default/':
+       # ensure => present,
         content => {
       'name'  => 'cluster-user',
        'value'=> '${jboss.partition.name:DefaultPartition}',
        }
        }   
        
-       wildfly::resource {'/subsystem=messaging-activemq/server=default':
+       wildfly::resource {'/subsystem=messaging-activemq/server=default/':
        ensure => present,
        content => {
       'name'  => 'cluster-password',
