921e1a0ff1f1c91700fadebcedfa0523a6f30134
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-02-09 14:03:45 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -207,13 +207,14 @@ wildfly::resource { '/subsystem=infinispan/cache-container=ejb/transport=TRANSPO
        }
        }
        
-    wildfly::resource { '/subsystem=infinispan/cache-container=web/replicated-cache=repl':
+    wildfly::resource { '/subsystem=infinispan/cache-container=web/replicated-cache=repl
+                         subsystem=infinispan/cache-container=web/replicated-cache=repl/store=file':
      #content => {
     # 'mode' => 'ASYNC'
      #}
      }
      
-   -> wildfly::resource { '/subsystem=infinispan/cache-container=web/replicated-cache=repl/store=file':
+ #  -> wildfly::resource { '/subsystem=infinispan/cache-container=web/replicated-cache=repl/store=file':
 
      }
 
