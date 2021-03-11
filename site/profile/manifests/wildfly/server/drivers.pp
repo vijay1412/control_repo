ce176ea848b69fadb76ce3d34868f3e1737d6b8b
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-03-11 12:37:59 -0600

    Update drivers.pp

--- site/profile/manifests/wildfly/server/drivers.pp
+++ site/profile/manifests/wildfly/server/drivers.pp
@@ -1,5 +1,5 @@
-profile::wildfly::server::drivers{
-}
+class profile::wildfly::server::drivers(
+){
 wildfly::config::module { 'org.postgresql':
   source       => 'https://repo1.maven.org/maven2/org/postgresql/postgresql/9.3-1103-jdbc4/postgresql-9.3-1103-jdbc4.jar',
   dependencies => ['javax.api', 'javax.transaction.api'],
@@ -10,4 +10,5 @@ wildfly::datasources::driver { 'Driver postgresql':
   driver_name                     => 'postgresql',
   driver_module_name              => 'org.postgresql',
   driver_xa_datasource_class_name => 'org.postgresql.xa.PGXADataSource'
+}
 }
