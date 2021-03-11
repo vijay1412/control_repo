86971cd593b12fd12f29f778e5fa2a3fe1bfc72b
vijay1412 <73511962+vijay1412@users.noreply.github.com>
2021-03-11 12:34:34 -0600

    Update install.pp

--- site/profile/manifests/wildfly/server/install.pp
+++ site/profile/manifests/wildfly/server/install.pp
@@ -255,11 +255,7 @@ wildfly::resource { '/subsystem=infinispan/cache-container=ejb/transport=TRANSPO
       wildfly::resource { '/subsystem=modcluster':  
       ensure => absent,
     }
-  wildfly::config::module { 'org.postgresql':
-  #source       => 'https://repo1.maven.org/maven2/org/postgresql/postgresql/9.3-1103-jdbc4/postgresql-9.3-1103-jdbc4.jar',
-  dependencies => ['javax.api', 'javax.transaction.api'],
-  require      => Class['wildfly'],
-}
+  
   }
 
  
